42c05b267807a8ae6816c98f777236a4be688325
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-03-15 22:26:55 -0700

    first commit

---
+++ webpack-css.config.js
@@ -0,0 +1,36 @@
+// webpack.config.js
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+module.exports = {
+    // The standard entry point and output config
+    entry: {
+        entry0: "./entry-0",
+        entry1: "./entry-1"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].js",
+        chunkFilename: "[id].js"
+    },
+    module: {
+        loaders: [
+            // Extract css files
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            // Optionally extract less files
+            // or any other compile-to-css language
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+            // You could also use other loaders the same way. I. e. the autoprefixer-loader
+        ]
+    },
+    // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
+    plugins: [
+        new ExtractTextPlugin("[name].css")
+    ]
+}
